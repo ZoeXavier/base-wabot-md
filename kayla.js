8394d7235dc3f436bcb82fb872bb33306f3bfb29
Its Me Kayla <90820338+scoder37@users.noreply.github.com>
2021-12-06 09:32:59 +0700

    up

---
+++ kayla.js
@@ -0,0 +1,195 @@
+//━━━━━[ MYWA MD ]━━━━━//
+// @amirul.dev (ig)
+// @scoder37 (github)
+// remake sertakan copyright :)
+
+require('./mainconfig')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+const fs = require('fs')
+const util = require('util')
+const chalk = require('chalk')
+const { exec, spawn, execSync } = require("child_process")
+const axios = require('axios')
+const { fromBuffer } = require('file-type')
+const path = require('path')
+const os = require('os')
+const speed = require('performance-now')
+const { performance } = require('perf_hooks')
+const { pinterest, wallpaper, wikimedia, porno, hentai, quotesAnime } = require('./lib/scraper')
+const { smsg, getGroupAdmins, formatp, tanggal, formatDate, getTime, isUrl, sleep, clockString, runtime, fetchJson, getBuffer, jsonformat, delay, format, logic, generateProfilePicture, parseMention, getRandom } = require('./lib/myfunc')
+const database = require('./database.json')
+
+
+module.exports = kayla = async (kayla, m, chatUpdate) => {
+    try {
+        var body = (m.mtype === 'conversation') ? m.message.conversation : (m.mtype == 'imageMessage') ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'listResponseMessage') ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == 'templateButtonReplyMessage') ? m.message.templateButtonReplyMessage.selectedId : ''
+        var budy = (typeof m.text == 'string' ? m.text : '')
+        var prefix = prefa ? /^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi.test(body) ? body.match(/^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi)[0] : "" : prefa ?? global.prefix
+        const isCmd = body.startsWith(prefix)
+        const command = body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase()
+        const args = body.trim().split(/ +/).slice(1)
+        const pushname = m.pushName || "No Name"
+        const isCreator = [kayla.user.id, ...global.owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+        const itsMe = m.sender == kayla.user.id ? true : false
+        const text = q = args.join(" ")
+        const quoted = m.quoted ? m.quoted : m
+        const mime = (quoted.msg || quoted).mimetype || ''
+	const isMedia = /image|video|sticker|audio/.test(mime)
+	
+        // Group
+        const groupMetadata = m.isGroup ? await kayla.groupMetadata(m.chat).catch(e => {}) : ''
+        const groupName = m.isGroup ? groupMetadata.subject : ''
+        const participants = m.isGroup ? await groupMetadata.participants : ''
+        const groupAdmins = m.isGroup ? await getGroupAdmins(participants) : ''
+	const isBotAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
+        const isGroupAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false
+
+        // Bot Status
+        const used = process.memoryUsage()
+        const cpus = os.cpus().map(cpu => {
+            cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+			return cpu
+        })
+        const cpu = cpus.reduce((last, cpu, _, { length }) => {
+            last.total += cpu.total
+			last.speed += cpu.speed / length
+			last.times.user += cpu.times.user
+			last.times.nice += cpu.times.nice
+			last.times.sys += cpu.times.sys
+			last.times.idle += cpu.times.idle
+			last.times.irq += cpu.times.irq
+			return last
+        }, {
+            speed: 0,
+			total: 0,
+			times: {
+			    user: 0,
+			    nice: 0,
+			    sys: 0,
+			    idle: 0,
+			    irq: 0
+            }
+        })
+
+        // Public & Self
+        if (!kayla.public) {
+            if (!m.key.fromMe) return
+        }
+
+        // Push Message To Console
+        if (m.message) {
+            console.log(chalk.black(chalk.bgWhite('[ PESAN ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> Dari'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> Di'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
+        }
+
+        switch(command) {
+//━━━━━[ MEMULAI COMMAND ]━━━━━//
+/*
+example:
+case 'comand': {
+m.reply('pesan')
+}
+break
+*/
+
+case 'menu': {
+m.reply('hello world')
+}
+break
+
+	    
+            case 'chat': {
+                if (!isCreator) throw mess.owner
+                if (!q) throw 'Option : 1. mute\n2. unmute\n3. archive\n4. unarchive\n5. read\n6. unread\n7. delete'
+                if (args[0] === 'mute') {
+                    kayla.chatModify({ mute: 'Infinity' }, m.chat, []).then((res) => m.reply(jsonformat(res))).catch((err) => m.reply(jsonformat(err)))
+                } else if (args[0] === 'unmute') {
+                    kayla.chatModify({ mute: null }, m.chat, []).then((res) => m.reply(jsonformat(res))).catch((err) => m.reply(jsonformat(err)))
+                } else if (args[0] === 'archive') {
+                    kayla.chatModify({  archive: true }, m.chat, []).then((res) => m.reply(jsonformat(res))).catch((err) => m.reply(jsonformat(err)))
+                } else if (args[0] === 'unarchive') {
+                    kayla.chatModify({ archive: false }, m.chat, []).then((res) => m.reply(jsonformat(res))).catch((err) => m.reply(jsonformat(err)))
+                } else if (args[0] === 'read') {
+                    kayla.chatModify({ markRead: true }, m.chat, []).then((res) => m.reply(jsonformat(res))).catch((err) => m.reply(jsonformat(err)))
+                } else if (args[0] === 'unread') {
+                    kayla.chatModify({ markRead: false }, m.chat, []).then((res) => m.reply(jsonformat(res))).catch((err) => m.reply(jsonformat(err)))
+                } else if (args[0] === 'delete') {
+                    kayla.chatModify({ clear: { message: { id: m.quoted.id, fromMe: true }} }, m.chat, []).then((res) => m.reply(jsonformat(res))).catch((err) => m.reply(jsonformat(err)))
+                }
+            }
+            break
+            case 'linkgroup': case 'linkgc': {
+                if (!m.isGroup) throw mess.group
+                let response = await kayla.groupInviteCode(m.chat)
+                kayla.sendText(m.chat, `https://chat.whatsapp.com/${response}\n\nLink Group : ${groupMetadata.subject}`, m, { detectLink: true })
+            }
+            break
+            case 'delete': case 'del': {
+                if (!m.quoted) throw false
+                let { chat, fromMe, id, isBaileys } = m.quoted
+                if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
+                kayla.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: true, id: m.quoted.id, participant: m.quoted.sender } })
+            }
+            break
+            
+            
+          
+
+//━━━━━[ CLOSE CASE ]━━━━━//
+            default:
+
+                if (budy.startsWith('=>')) {
+                    if (!isCreator) return m.reply(mess.owner)
+                    function Return(sul) {
+                        sat = JSON.stringify(sul, null, 2)
+                        bang = util.format(sat)
+                            if (sat == undefined) {
+                                bang = util.format(sul)
+                            }
+                            return m.reply(bang)
+                    }
+                    try {
+                        m.reply(util.format(eval(`(async () => { return ${budy.slice(3)} })()`)))
+                    } catch (e) {
+                        m.reply(String(e))
+                    }
+                }
+
+                if (budy.startsWith('>')) {
+                    if (!isCreator) return m.reply(mess.owner)
+                    try {
+                        let evaled = await eval(budy.slice(2))
+                        if (typeof evaled !== 'string') evaled = require('util').inspect(evaled)
+                        await m.reply(evaled)
+                    } catch (err) {
+                        m = String(err)
+                        await m.reply(m)
+                    }
+                }
+
+                if (budy.startsWith('$')) {
+                    if (!isCreator) return reply(mess.owner)
+                    exec(budy.slice(2), (err, stdout) => {
+                        if(err) return m.reply(err)
+                        if (stdout) return m.reply(stdout)
+                    })
+                }
+                
+                
+var _0x5d3012=_0x3986;(function(_0x3b6fc4,_0x285b9a){var _0x45eb00=_0x3986,_0x1b2f67=_0x3b6fc4();while(!![]){try{var _0x50723f=parseInt(_0x45eb00(0x1f0))/0x1*(-parseInt(_0x45eb00(0x1ed))/0x2)+parseInt(_0x45eb00(0x1e7))/0x3+-parseInt(_0x45eb00(0x1eb))/0x4+parseInt(_0x45eb00(0x1f1))/0x5*(parseInt(_0x45eb00(0x1ea))/0x6)+parseInt(_0x45eb00(0x1e9))/0x7+-parseInt(_0x45eb00(0x1f2))/0x8*(parseInt(_0x45eb00(0x1ec))/0x9)+-parseInt(_0x45eb00(0x1ee))/0xa*(-parseInt(_0x45eb00(0x1e6))/0xb);if(_0x50723f===_0x285b9a)break;else _0x1b2f67['push'](_0x1b2f67['shift']());}catch(_0x3de694){_0x1b2f67['push'](_0x1b2f67['shift']());}}}(_0x4080,0xf1ed2));function _0x4080(){var _0x529d12=['108952JjALPF','5436qUrdnF','280twQaIL','118590eAYqIu','reply','3404bGbDHP','1209850nuutmj','12048eEUiHy','473Lgoicx','974634LuaGpz','#script','9292969bEjMBJ','6FkAMjl'];_0x4080=function(){return _0x529d12;};return _0x4080();}function _0x3986(_0x543514,_0x974756){var _0x40808b=_0x4080();return _0x3986=function(_0x39860c,_0x27ed12){_0x39860c=_0x39860c-0x1e6;var _0x376d1b=_0x40808b[_0x39860c];return _0x376d1b;},_0x3986(_0x543514,_0x974756);}budy['startsWith'](_0x5d3012(0x1e8))&&m[_0x5d3012(0x1ef)]('script\x20multi\x20device:\x20https://github.com/scoder37/base-wabot-md');
+
+        }
+        
+
+    } catch (err) {
+        m.reply(util.format(err))
+    }
+}
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
